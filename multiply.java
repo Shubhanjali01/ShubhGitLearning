5c4467b719d10a495b015580d215814bba4266a4
Shubhanjali <iit2021230@iiita.ac.in>
2023-06-11 09:25:53 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,6 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=9;int b=5;
+        System.out.println("product of a: " +a+ " and  b:"+b+ " is :  " +a*b);
+    }
+}
